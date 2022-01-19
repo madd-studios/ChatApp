c0c787eccb874a6de7deab52c1b1da5846472d50
StevenB <sbartkiewicz@gmail.com>
2022-01-18 21:57:32 -0500

    Got a basic socket.io connection working

---
+++ public/client.js
@@ -0,0 +1,35 @@
+window.onload = () => {
+
+    const clientSocket = io();
+
+    const chatWindow = document.getElementsByClassName("chat-window")[0];
+
+    clientSocket.on("connect", (socket) => {
+
+        const message = createConnectionMessage();
+
+        chatWindow.appendChild(message);
+
+    });
+
+    function createConnectionMessage() {
+        const messageContainerReceived = document.createElement("div");
+        messageContainerReceived.className = "message-container-received";
+        const messageReceived = document.createElement("div");
+        messageReceived.className = "message-received";
+        const messager = document.createElement("span");
+        messager.className = "messager";
+        messager.innerHTML = "Computer";
+        const message = document.createElement("span");
+        message.innerHTML = "Connected to server!";
+
+        messageReceived.appendChild(messager);
+        messageReceived.appendChild(message);
+        messageContainerReceived.appendChild(messageReceived);
+
+        return messageContainerReceived;
+
+    }
+
+}
+
---
+++ server.js
@@ -0,0 +1,67 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const eiows = require("eiows");
+const fs = require('fs');
+
+const httpServer = createServer((req, res) => {
+    
+    res.setHeader('Content-Type', 'text/html');
+
+    // routing
+    let path = './public/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/chat':
+            path += 'chat.html';
+            res.statusCode = 200;
+            break;
+        // CSS Files
+        case '/styles.css':
+            res.setHeader('Content-Type', 'text/css')
+            path = 'styles.css'
+            res.statusCode = 200;
+            break;
+        // JS Files
+        case '/client.js':
+            res.setHeader('Content-Type', 'text/javascript')
+            path += 'client.js';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+    }
+    console.log(`PATH: ${path}`);
+    // response
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log("IN readFile");
+            console.log(err.message);
+            res.end();
+        }
+        else {
+            res.end(data);
+        }
+    });
+
+    path = "";
+
+});
+
+const io = new Server(httpServer);
+
+// io.on("connection", (socket) => {
+//     console.log("Client successfully connected");
+// });
+
+httpServer.listen(5500, 'localhost', () => {
+    console.log("Listening on port 5500");
+});
+
+// httpServer.listen(5500);
+
+
+
